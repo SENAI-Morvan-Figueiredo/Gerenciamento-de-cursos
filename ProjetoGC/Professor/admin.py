3b4ed208584694265e0a5e38112f3a53092b092e
Ed_gomes <edgargomesoliveira448@gmail.com>
2025-10-06 14:55:31 -0300

    Admins adicionados

--- ProjetoGC/Professor/admin.py
+++ ProjetoGC/Professor/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Aula, Frequencia
 
-# Register your models here.
+admin.site.register(Aula)
+admin.site.register(Frequencia)
+
+class FrequenciaAdmin(admin.ModelAdmin):
+    list_display = ('aula', 'matricula', 'presenca')
+    search_fields = ('aula', 'matricula', 'presenca')
+
+class AulaAdmin(admin.ModelAdmin):
+    list_display = ('data', 'tipo', 'turma')
+    search_fields = ('data', 'tipo', 'turma')
+    # Register your models here.
